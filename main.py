7032d95aa3682e2532a019881e0d9c419c6fe7e0
Divyansh Arora <158808168+divA2805@users.noreply.github.com>
2025-09-13 01:14:47 +0530

    Change default value of --n_his from 2 to 3

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def get_parameters():
     parser.add_argument('--enable_cuda', type=bool, default=True, help='enable CUDA, default as True')
     parser.add_argument('--seed', type=int, default=42, help='set the random seed for stabilizing experiment results')
     parser.add_argument('--dataset', type=str, default='metr-la', choices=['metr-la', 'pems-bay', 'pemsd7-m'])
-    parser.add_argument('--n_his', type=int, default=2)
+    parser.add_argument('--n_his', type=int, default=3)
     parser.add_argument('--n_pred', type=int, default=1, help='the number of time interval for predcition, default as 1')
     parser.add_argument('--time_intvl', type=int, default=5)
     parser.add_argument('--Kt', type=int, default=3)
